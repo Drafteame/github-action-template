25c1af3403d6f736b2ade4f4dc820de545bae369
Eduardo Aguilar <dante.aguilar41@gmail.com>
2024-05-21 15:03:24 -0600

    chore: main index file

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-import core from '@actions/core';
-
 import Action from './src/action.js';
+import * as inputs from "./src/inputs/index.js";
 
 const main = async () => {
   const action = new Action(
-    core.getInput('action'),
-    core.getInput('file_path'),
-    core.getInput('private_key'),
-    core.getInput('out_file')
+    inputs.getString('action', 'default')
   );
 
-  try {
-    await action.run();
-  } catch (e) {
-    core.error(
-      `[ERROR] Failure on ejson ${core.getInput('action')}: ${e.message}`
-    );
-
-    process.exit(1);
-  }
+  action.run();
 };
 
 main();
